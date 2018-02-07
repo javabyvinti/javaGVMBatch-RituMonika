8240d37ff7afc013b5900bf2abcb70de4e1ea5b8
javabyvinti <32234213+javabyvinti@users.noreply.github.com>
2018-02-07 13:41:30 +0530

    Add files via upload

---
+++ 7 Feb - Java class data/Sum.java
@@ -0,0 +1,37 @@
+// sum of 2 values -->
+
+
+class Sum{
+
+	public static void main(String args[])
+	{
+		// create an object
+
+	Sum obj = new Sum();
+	obj.main();
+	}
+	public static void main()
+	{
+		int first = 30;
+		int second = 60;
+		int sum = first + second;
+		
+		//System --> Predefined class
+		// out --> its an object of PrintStream class
+		// println() -->predefined function--> to print data
+		
+
+		System.out.println(sum); 
+
+
+
+	// by default --> console window
+		
+	// in object 
+	// input and output
+	// print --> console, file, network
+
+	// inheritance
+	}
+	
+} 
---
+++ 7 Feb - Java class data/first.java
@@ -0,0 +1,22 @@
+class hello{
+	// public --> access --> data access
+	// static --> one time memory allocation
+
+	// no need of creating object for calling main 
+
+	static public void main(String[] ar)	// java 1.5 version
+	{
+		// String args[]
+		// int value[]
+		System.out.println("Hello Java");
+	}
+	
+	// returntype functionname(no. of arguments) --> void,int, char, float
+
+}
+// java compiler --> javac FileName.java
+// java run --> jvm --> java ClassName
+
+
+
+	// 90443434365 --> String --> char
